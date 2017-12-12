626a9972c968793582ad34088b3f073e0ee355c1
Yohei Omura <yomura7@gmail.com>
2017-12-12 23:44:47 +0900

    add config.cson

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,3 +3,4 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.ideavimrc ~/.ideavimrc
 ln -sf ~/dotfiles/package.txt ~/.atom/package.txt
 ln -sf ~/dotfiles/keymap.cson ~/.atom/keymap.cson
+ln -sf ~/dotfiles/config.cson ~/.atom/config.cson
