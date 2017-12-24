f484dcb621776eade5f9ec0ecd444fa01c195570
Yohei Omura <yomura7@gmail.com>
2017-12-24 23:46:29 +0900

    remove config.cson

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -4,5 +4,4 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.ideavimrc ~/.ideavimrc
 ln -sf ~/dotfiles/package.txt ~/.atom/package.txt
 ln -sf ~/dotfiles/keymap.cson ~/.atom/keymap.cson
-ln -sf ~/dotfiles/config.cson ~/.atom/config.cson
 source ~/.bash_profile
